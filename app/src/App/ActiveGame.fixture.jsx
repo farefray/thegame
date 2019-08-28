681650a0bc3d8e53453993023e9f3382eddf5e87
Unknown <maksym.fedan@gmail.com>
2019-08-28 23:34:16 +0300

    make it great again please

--- app/src/App/ActiveGame.fixture.jsx
+++ app/src/App/ActiveGame.fixture.jsx
@@ -3,72 +3,80 @@ import { ReduxMock } from 'react-cosmos-redux';
 import rootReducer from '../reducers';
 import { createStore } from 'redux';
 import _ from 'lodash';
-import { useDispatch } from 'react-redux'
+import { useDispatch } from 'react-redux';
 
 import ActiveGame from './ActiveGame';
 
-const ConnectedPlayers = require('../../../src/models/ConnectedPlayers.js');
-const GameController = require('../../../src/game.js');
-const BattleController = require('../../../src/game/battle.js');
-const Customer = require('../../../src/objects/Customer.js');
-const MOCK_SOCKETID_1 = 'MOCK_SOCKETID_1';
-
-const MOCK_CLIENTS = [MOCK_SOCKETID_1];
+const BoardJS = require('../../../src/game/board.js');
+const Battle = require('../../../src/objects/Battle.js');
 
 // todo make it share functionality with jest and core.test.js
-const generateGameState = async function() {
-	const connectedPlayers = new ConnectedPlayers();
-	connectedPlayers.set(MOCK_SOCKETID_1, new Customer(MOCK_SOCKETID_1));
-	connectedPlayers.setIn(MOCK_SOCKETID_1, ['isReady', true]);
-	
-	let gameState = await GameController.initialize(MOCK_CLIENTS);
-	gameState = await GameController.purchasePawn(gameState, MOCK_SOCKETID_1, 0);
-	const fromPosition = '0';
-	const toPosition = '0,2';
-	await GameController.mutateStateByPawnPlacing(gameState, MOCK_SOCKETID_1, fromPosition, toPosition);
-    console.log("TCL: preBattleState", gameState)
-	const battleRoundResult = await BattleController.setup(gameState);
-	return battleRoundResult;
+const generateGameState = async function(boards) {
+	const npcBoard = await BoardJS.createBoard(boards.A);
+	const playerBoard = await BoardJS.createBoard(boards.B);
+
+	const combinedBoard = await BoardJS.createBattleBoard(playerBoard, npcBoard);
+	const battle = new Battle(combinedBoard);
+	const battleResult = await battle.execute();
+	return battleResult;
 };
 
-// Put this somewhere central (chances are you'll have some utils thingie)
-const MyReduxMock = ({ children, initialState }) => {
-	console.log("TCL: MyReduxMock -> initialState", initialState)
-	
+const MyReduxMock = ({ children, initialState, boards }) => {
 	return (
 		<ReduxMock configureStore={state => createStore(rootReducer, state)} initialState={initialState}>
-			<MyReduxContext initialState={initialState}>
-			{children}
+			<MyReduxContext initialState={initialState} boards={boards}>
+				{children}
 			</MyReduxContext>
 		</ReduxMock>
 	);
 };
 
-const MyReduxContext = ({ children, initialState }) => {
+const MyReduxContext = ({ children, initialState, boards }) => {
 	const dispatch = useDispatch();
 
 	React.useEffect(() => {
-		generateGameState().then(battleRoundResult => {
-			// const newState = _.cloneDeep(initialState);
-			// newState.app.actionStack = battleRoundResult.actionStack;
-			// newState.app.isActiveBattleGoing = true;
-			// dispatch({
-			// 	type: 'DEBUG',
-			// 	newState
-			// });
+		generateGameState(boards).then(battleRoundResult => {
+			console.log('TCL: MyReduxContext -> battleRoundResult', battleRoundResult);
+			const newState = _.cloneDeep(initialState);
+			dispatch({
+				type: 'DEBUG',
+				newState
+			});
 
-			dispatch({ type: 'BATTLE_TIME', actionStack: battleRoundResult.battles[MOCK_SOCKETID_1].actionStack, startBoard:battleRoundResult.battles[MOCK_SOCKETID_1].startBoard, winner:battleRoundResult.battles[MOCK_SOCKETID_1].winner});
+			dispatch({
+				type: 'BATTLE_TIME',
+				actionStack: battleRoundResult.actionStack,
+				startBoard: battleRoundResult.startBoard,
+				winner: battleRoundResult.winner
+			});
 		});
 	}, []);
 
 	return children;
-}
+};
 
 export default () => {
 	const baseState = rootReducer({}, { type: 'INIT' });
 
+	const [boards, setBoards] = React.useState({
+		A: [
+			{
+				name: 'minotaur',
+				x: 1,
+				y: 8
+			}
+		],
+		B: [
+			{
+				name: 'minotaur',
+				x: 3,
+				y: 4
+			}
+		]
+	});
+
 	return (
-		<MyReduxMock initialState={baseState}>
+		<MyReduxMock initialState={baseState} boards={boards}>
 			<ActiveGame />
 		</MyReduxMock>
 	);
