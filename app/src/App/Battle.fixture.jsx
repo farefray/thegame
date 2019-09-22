4dbf97c226e575d22461e3773584f485fb185c53
Maksym Fedan <maksym.fedan@gmail.com>
2019-09-22 22:40:34 +0300

    fixing fixtures

--- app/src/App/Battle.fixture.jsx
+++ app/src/App/Battle.fixture.jsx
@@ -35,7 +35,7 @@ const generateBoard = () => {
   const unitCount = 7;
   const board = [];
   while (board.length < unitCount) {
-    const x = Math.floor(Math.random() * 8);
+    const x = Math.floor(Math.random() * 7);
     const y = Math.floor(Math.random() * 4);
     if (board.find(unit => unit.x === x && unit.y === y)) continue;
     board.push({
@@ -49,8 +49,8 @@ const generateBoard = () => {
 
 const flipBoard = board => {
   for (const unit of board) {
-    unit.y = 7 - unit.y;
-    unit.x = 7 - unit.x;
+    unit.y = 6 - unit.y;
+    unit.x = 6 - unit.x;
     unit.name = 'dwarf';
   }
   return board;
