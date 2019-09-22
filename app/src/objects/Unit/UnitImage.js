62398c1477f88b0acb4664635f507b092481c113
Maksym Fedan <maksym.fedan@gmail.com>
2019-09-22 23:56:22 +0300

    units turns and moving animation(todo move to local and cache)

--- app/src/objects/Unit/UnitImage.js
+++ app/src/objects/Unit/UnitImage.js
@@ -6,6 +6,10 @@ import fallbackImage from '../../assets/monsters/default.png';
 export default function UnitImage({ lookType, direction, isMoving }) {
   const [sprite, setSprite] = React.useState(getPawnImageSrc(lookType, direction, !isMoving));
 
+  React.useEffect(() => {
+    setSprite(getPawnImageSrc(lookType, direction, !isMoving));
+  }, [setSprite, direction, isMoving, lookType]);
+
   return (
     <img src={sprite} onError={() => setSprite(fallbackImage)} alt="Unit" style={{ position: 'absolute', bottom: 0, right: 0 }} />
   );
