ab3c5018feac09e7c85aa1c6c79149cec560d3a4
Maksym Fedan <maksym.fedan@gmail.com>
2019-09-22 22:27:40 +0300

    some experiments with packages and upgrades

--- app/config-overrides.js
+++ app/config-overrides.js
@@ -1,5 +1,7 @@
 const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
-
+/**
+ * Used for react cosmos, in order to include backend files from directories of backend
+ */
 module.exports = function override(config, env) {
   config.resolve.plugins = config.resolve.plugins.filter(plugin => !(plugin instanceof ModuleScopePlugin));
 
