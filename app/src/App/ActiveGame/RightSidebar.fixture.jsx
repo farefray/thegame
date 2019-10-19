9b07d2f5b53f80029f642b6f38b561f99446f84a
Jacek <jacek.kizinski@hotmail.com>
2019-10-19 17:50:08 +0200

    Fix fixtures

--- app/src/App/ActiveGame/RightSidebar.fixture.jsx
+++ app/src/App/ActiveGame/RightSidebar.fixture.jsx
@@ -1,9 +1,9 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { ReduxMock } from 'react-cosmos-redux';
 import rootReducer from '../../reducers';
 import { createStore } from 'redux';
 import RightSidebar from './RightSidebar';
-import GameController from '../../../../controllers/GameController.js';
+import GameController from '../../../../src/controllers/GameController';
 
 // todo make it share functionality with jest and core.test.js
 const getCircularReplacer = () => {
