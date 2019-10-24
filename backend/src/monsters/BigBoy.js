dd8e70f41f5a4a8b5771e19885f691b5bb560dd4
fare <maksym.fedan@gmail.com>
2019-10-24 21:27:49 +0300

    Return of BigBoy

---
+++ backend/src/monsters/BigBoy.js
@@ -0,0 +1,18 @@
+import Monster from '../abstract/Monster';
+
+function BigBoy() {
+  return new Monster({
+    lookType: 25,
+    cost: 1,
+    hp: 3750,
+    mana: 0,
+    attack: 50,
+    attackRange: 1,
+    armor: 5,
+    speed: 1000,
+    attackSpeed: 1000,
+    manaRegen: 10
+  });
+}
+
+export default BigBoy;
