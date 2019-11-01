b4eeab808e9515152de873a03aa13c3a2ae34e55
Maksym Fedan <maksym.fedan@gmail.com>
2019-11-01 21:30:38 +0200

    kind of working side effects for battle

--- src/spells/Regeneration.js
+++ src/spells/Regeneration.js
@@ -4,8 +4,8 @@ function Regeneration(self) {
   const caster = self.caster;
   return {
     canBeCast: ({ units }) => {
-      // move to some abstract thing, like spellUtils.getClosestTarget(amount: 1, enemy: true)
-      const target = pathUtils.getClosestTarget({ x: caster.x, y: caster.y, targets: units.filter(u => u.team === self.caster.oppositeTeam() && u.isAlive()) });
+      // move to some abstract thing, like spellUtils.getClosestTarget(amount: 1, enemy: false)
+      const target = pathUtils.getClosestTarget({ x: caster.x, y: caster.y, targets: units.filter(u => u.team === self.caster.team && u.isAlive()) });
 
       if (!target) {
         return false;
@@ -18,16 +18,18 @@ function Regeneration(self) {
       // initial cast
       const target = self.props.target;
       const regenValue = caster.spellconfig.value;
-      target.healthChange(regenValue);
+      target.healthChange(regenValue, caster.id);
 
       // side effect [todo]
-      self.props.addSideEffect({
+      self.props.addSideEffect(caster, {
         effect: () => {
-          target.healthChange(regenValue);
+          target.healthChange(regenValue, caster.id);
         },
         tick: 1000,
         amount: 15
       });
+
+      return true;
     }
   };
 }
