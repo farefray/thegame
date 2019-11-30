e77b72e354792df5a92c7c91a4c5049a8f79cffb
Maksym Fedan <maksym.fedan@gmail.com>
2019-11-30 13:15:20 +0200

    refactoring for particles, preparing other effects

---
+++ frontend/src/objects/Unit/Effect_C.ts
@@ -0,0 +1,29 @@
+const uuidv1 = require('uuid/v1');
+
+interface EffectPosition {
+    top: number;
+    left: number;
+}
+
+interface EffectConfig {
+    lookType: string;
+    speed: number;
+    from: EffectPosition;
+    to?: EffectPosition;
+}
+
+export default class Effect_C {
+    public id: string;
+    public lookType: string;
+    public speed: number;
+    public from: EffectPosition;
+    public to: EffectPosition | undefined;
+  
+    constructor (econfig: EffectConfig) {
+        this.id = uuidv1();
+        this.lookType = econfig.lookType;
+        this.speed = econfig.speed;
+        this.from = econfig.from;
+        this.to = econfig.to;
+    }
+}
