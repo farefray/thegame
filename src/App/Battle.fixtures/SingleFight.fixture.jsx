9dfa9926836557c7969f1715f08772989706a967
Maksym Fedan <maksym.fedan@gmail.com>
2020-05-23 12:40:12 +0300

    revised spell system a lil bit

--- src/App/Battle.fixtures/SingleFight.fixture.jsx
+++ src/App/Battle.fixtures/SingleFight.fixture.jsx
@@ -7,7 +7,7 @@ const defaultBoard = [{
   owner: 'first_player',
   units: new BattleUnitList([
     new BattleUnit({
-      name: 'dwarf',
+      name: 'minotaur_guard',
       x: 5,
       y: 0
     })
@@ -17,7 +17,7 @@ const defaultBoard = [{
   owner: 'second_player',
   units: new BattleUnitList([
     new BattleUnit({
-      name: 'dwarf',
+      name: 'dwarf_geomancer',
       x: 5,
       y: 7
     }),
