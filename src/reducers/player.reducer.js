032a1954fbfe73b19ceb71e75c5f5da9ef51d58c
Maksym Fedan <maksym.fedan@gmail.com>
2020-06-27 00:00:33 +0300

    now sending battle results via eventbus also

--- src/reducers/player.reducer.js
+++ src/reducers/player.reducer.js
@@ -2,8 +2,6 @@ const basePlayerState = {
   health: 100,
   mana: 0,
   level: -1,
-  exp: -1,
-  expToReach: -1,
   gold: -1,
   handUnits: [],
   boardUnits: []
@@ -15,8 +13,6 @@ export default function player(state = basePlayerState, action) {
       return state = {
         ...state,
         level: action.player.level,
-        exp: action.player.exp,
-        expToReach: action.player.expToReach,
         gold: action.player.gold,
         handUnits: action.player.hand,
         boardUnits: action.player.board,
